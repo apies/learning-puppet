103a4aee4f372c7974ccd47f5b6f5a662be99ae0
Alan Pies <alan.lee.pies@gmail.com>
2012-11-12 11:21:39 -0800

    using puppetlabs postgresql module, split nginx config into its own module

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,20 +1,32 @@
 Exec {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin/:sbin:/bin"
+  
+   
 	
 }
-package {
-	"nginx":
-	  ensure => present
+
+include nginx
+
+
+class { 'postgresql::server':
+   config_hash => {
+       'ip_mask_allow_all_users'    => '127.0.0.1/0',
+       'listen_addresses'           => '*',
+       'manage_redhat_firewall' => true,
+       'postgres_password'          => 'postgres',
+       'ip_mask_deny_postgres_user' => '192.169.9.999'
+   }
 }
-service {
-	"nginx":
-	  ensure => true,
-	  enable => true
-}
-file {
-	"/etc/nginx/nginx.conf":
-	source => "puppet:///modules/nginx/nginx.conf",
-	mode => 644,
-	owner => root,
-	group => root
+
+#  postgresql::db { 'mydb':
+#    user     => 'my_user',
+#    password => 'password',
+#    grant    => 'all'
+#  }
+
+
+
+postgresql::database_user {
+  'apies':
+    password_hash => '$1$zzVHWC1w$BZz0T4XLK5JRw0QDN97GW1'
 }
